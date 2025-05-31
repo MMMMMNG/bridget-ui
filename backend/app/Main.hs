baacb4ee4196983d2191533c65b031ccd0b5486d
MMMMMNG <113881019+MMMMMNG@users.noreply.github.com>
2025-05-31 15:35:10 +0200

    trying to access Main within unnamed package, no luck
    according to gemini, this is because only other members of the "unnamed package" can access the Main class.

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -19,26 +19,9 @@ main = do
     let jarPath = Runfiles.rlocation r "bridget_ui_ws/jar_deploy.jar"
         jvmArgs = [ "-Djava.class.path=" <> fromString jarPath ]
     withJVM jvmArgs $ handle (showException >=> Text.putStrLn) [java| {
-        java.util.List<java.lang.ClassLoader> classLoadersList = new java.util.LinkedList<java.lang.ClassLoader>();
-        classLoadersList.add(org.reflections.util.ClasspathHelper.contextClassLoader());
-        classLoadersList.add(org.reflections.util.ClasspathHelper.staticClassLoader());
 
-
-        org.reflections.Reflections reflections = new org.reflections.Reflections(
-            new org.reflections.util.ConfigurationBuilder()
-                .setScanners(
-                    new org.reflections.scanners.SubTypesScanner(false /* don't exclude Object.class */),
-                    new org.reflections.scanners.ResourcesScanner()
-                )
-                .setUrls(org.reflections.util.ClasspathHelper.forClassLoader(classLoadersList.toArray(new java.lang.ClassLoader[0])))
-                .filterInputsBy(new org.reflections.util.FilterBuilder().includePackage("org.bridget"))
-        );
-
-        java.lang.String s = reflections.getSubTypesOf(java.lang.Object.class).toString();
-      
-        System.out.println("Subtypes found: " + s); // Added for demonstration of output
-      int[][] blockShape = org.bridget.Main.tBlockShape;
-      org.bridget.PieceInventory pi = new org.bridget.PieceInventory();
+      int[][] blockShape = Main.tBlockShape;
+      PieceInventory pi = new PieceInventory();
       System.out.println(pi.hasPiece("L"));
       System.out.println(blockShape.toString());
       }
