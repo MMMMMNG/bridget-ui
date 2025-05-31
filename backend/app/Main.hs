e96193154cafd93fd4ded72b269d05d4876f4c54
MMMMMNG <113881019+MMMMMNG@users.noreply.github.com>
2025-05-31 12:54:14 +0200

    copy classpath example haskell code from inline-java

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -1,19 +1,31 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
-{-# OPTIONS_GHC -fplugin=Language.Java.Inline.Plugin #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Main where
 
-import qualified Data.Text as Text
+import Control.Exception (handle)
+import Data.String (fromString)
+import Control.Monad ((>=>))
+import qualified Bazel.Runfiles as Runfiles
 import qualified Data.Text.IO as Text
-import Foreign.JNI (withJVM)
-import Language.Java (reify, reflect)
+import Foreign.JNI (showException, withJVM)
 import Language.Java.Inline
 
+
 main :: IO ()
-main = withJVM [] $ do
-    text <- reflect (Text.pack "Java: Hello!")
-    jarray <- [java| {
-      System.out.println($text);
-      return new String[] {"Haskell:", "Hello", "Java but from bridget-ui!" };
-      } |]
-    xs <- reify jarray
-    Text.putStrLn (Text.unwords xs)
+main = do
+    r <- Runfiles.create
+    let jarPath = Runfiles.rlocation r "io_tweag_inline_java/examples/classpath/jar_deploy.jar"
+        jvmArgs = [ "-Djava.class.path=" <> fromString jarPath ]
+    withJVM jvmArgs $ handle (showException >=> Text.putStrLn) [java| {
+      org.apache.commons.collections4.OrderedMap map =
+        new org.apache.commons.collections4.map.LinkedMap();
+      map.put("FIVE", "5");
+      map.put("SIX", "6");
+      map.put("SEVEN", "7");
+      System.out.println(map.firstKey());
+      System.out.println(map.nextKey("FIVE"));
+      System.out.println(map.nextKey("SIX"));
+      }
+   |]
