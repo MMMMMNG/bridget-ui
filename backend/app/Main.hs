ca86faee93869bce76086d17b2c3e7c0b582a838
MMMMMNG <113881019+MMMMMNG@users.noreply.github.com>
2025-06-01 08:40:15 +0200

    put string from browser through the JVM back to the browser
    took me way too long to realize that reify wouldn't work for type String, but Data.Text

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -8,10 +8,11 @@ import Control.Exception (handle)
 import Data.String (fromString)
 import Control.Monad ((>=>))
 import qualified Bazel.Runfiles as Runfiles
-import qualified Data.Text.IO as Text
+import qualified Data.Text as DT
+import qualified Data.Text.IO as DTIO
 import Foreign.JNI (showException, withJVM, runInAttachedThread)
 import Language.Java.Inline
---import Language.Java (reflect)
+import Language.Java (reflect)
 import Language.Java (reify)
 
 import Web.Scotty
@@ -20,33 +21,50 @@ import qualified Data.Text.Lazy as TL
 import Control.Concurrent (runInBoundThread)
 
 
+putStringThroughJVM :: String -> IO DT.Text
+putStringThroughJVM thestr = runInBoundThread $ runInAttachedThread $ do
+    rn <- reflect (DT.pack (thestr <> " woah dude"))
+    s <- [java| {
+                String strr = $rn;
+                return String.join(", ", strr, "yay"); 
+            }
+        |]
+    ss <- reify s
+    pure (ss :: DT.Text)
+
+scottyPutStringJVM :: String -> ActionM DT.Text
+scottyPutStringJVM a = liftIO $ putStringThroughJVM a
+
+
+
+
+
 main :: IO ()
 main = do
     r <- Runfiles.create
     let jarPath = Runfiles.rlocation r "bridget_ui_ws/jar_deploy.jar"
         jvmArgs = [ "-Djava.class.path=" <> fromString jarPath ]
     withJVM jvmArgs $ do
-        handle (showException >=> Text.putStrLn) [java| { 
-                    System.out.println("Hi Haskell!");
+        st <- reflect (DT.pack "HENLO")
+        handle (showException >=> DTIO.putStrLn) [java| { 
+                    System.out.println($st);
                 } 
             |]
+
+        rr <- putStringThroughJVM "NAOAOAO"
+        DTIO.putStrLn rr
 
         liftIO $ scotty 3000 $ do
             get "/" $ do
                 html "<h1>Welcome To bridget ui!</h1>"
 
             get "/greet/:name" $ do
-                name <- pathParam "name" :: ActionM String
-                --let rn = reflect name
-                -- Interact with JVM to process the name
-                -- Note: `java` quasi-quote implicitly uses the JVM context from `withJVM`
-                -- `liftAndCatchIO` is used to run `IO` actions (like `java`) within Scotty's `ActionM` monad
-                javaMessage <- liftIO $ (runInBoundThread . runInAttachedThread) (do
-                    s <- [java| 0.1 + 0.2 |]
-                    ss <- reify s
-                    pure (ss :: Double)
-                    )
+                name <- pathParam "name"
+                liftIO $ putStrLn (name :: String)
 
-                html $ "<h2>" <> TL.pack (show javaMessage) <> TL.pack name <> "</h2>"
+
+                javaMessage <- scottyPutStringJVM "egz" 
+
+                html $ "<h2>" <> TL.fromStrict javaMessage <> TL.pack name <> "</h2>"
 
     -- The `scotty` function runs indefinitely.
